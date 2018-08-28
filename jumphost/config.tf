31612f8c0e2d3c5f279d4d87ac8f3e9645fda466
kneth <itsmekaush.4567@gmail.com>
2018-08-28 06:02:32 +0000

    Spinned up a instance and attached security groups

---
+++ jumphost/config.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  access_key = "AKIAIPSAZVLBQBDNWBYQ"
+  secret_key = "PZDN+6gjeBAMPAljzLFVkQZ7rcoTZtAdqdQkGZH9"
+  region     = "us-west-2"
+}
--- jumphost/main.tf
+++ jumphost/main.tf
@@ -1,1 +1,23 @@
-# This is the sampler TF file
+resource "aws_instance" "jump" {
+  ami           = "ami-6cd6f714"
+  instance_type = "t2.micro"
+#  name = "jump-i"
+}
+
+resource "aws_security_group" "jump" {
+  name = "jump-sg"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["10.0.0.0/21"]
+  }
+
+  egress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["10.0.0.0/21"]
+  }
+}
